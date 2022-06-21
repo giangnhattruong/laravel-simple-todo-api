bd9b8591b7cd6bc94dfe6981108e7c406c1451bc
GIANG NHAT TRUONG <giangnhattruong@gmail.com>
2022-06-21 09:20:57 +0700

    Fix bugs

--- app/Http/Resources/ColorResource.php
+++ app/Http/Resources/ColorResource.php
@@ -15,6 +15,7 @@ class ColorResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
         ];
     }
